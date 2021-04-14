a29e92fc593548510bbce9a7871680c16e658ec4
Xhulian Llani <xhulianl@uia.no>
2021-04-14 23:39:35 +0200

    added TodoListManager class

--- TodoList/app/src/main/java/com/example/todolist/data/TodoListManager.kt
+++ TodoList/app/src/main/java/com/example/todolist/data/TodoListManager.kt
@@ -1,4 +1,30 @@
 package com.example.todolist.data
 
 class TodoListManager {
+    private lateinit var todolistCollection: MutableList<TodoList>
+
+    var onLists: ((List<TodoList>) -> Unit)? = null
+    var onListUpdate: ((list: TodoList) -> Unit)? = null
+
+    fun load(){
+        todolistCollection = mutableListOf(
+            TodoList("Liste 1"),
+            TodoList("Liste 2"),
+            TodoList("Liste 3")
+        )
+        onLists?.invoke(todolistCollection)
+    }
+
+    fun updateList(list: TodoList){
+        onListUpdate?.invoke(list)
+    }
+
+    fun addList(list: TodoList){
+        todolistCollection.add(list)
+        onLists?.invoke(todolistCollection)
+    }
+
+    companion object{
+        val instance = TodoListManager()
+    }
 }
